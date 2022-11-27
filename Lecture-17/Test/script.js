e695d381150a86db57ce86c96c0a1720476d5869
Sabeel Khan <sabeelhps@gmail.com>
2022-11-27 11:00:57 +0530

    Lecture-17(Practice)

---
+++ Lecture-17/Test/script.js
@@ -0,0 +1,107 @@
+// const r = 2;
+
+// const area = Math.PI * r * r;
+
+// console.log(area);
+
+const radii = [2, 3, 1, 4, 6, 7, 8];
+
+function areas(radii) {
+    const res = [];
+    for (let radius of radii) {
+        const area = Math.PI * radius * radius;
+        res.push(area);
+    }
+
+    return res;
+}
+
+// console.log(areas(radii));
+
+
+// ---------------------------reverse words--------
+
+
+const sentence = "The blue fox jumps over a white lazy dog";
+
+// expected ans = "dog lazy white a over jumps fox blue The";
+
+
+function reverseSentence(sentence) {
+    
+}
+
+
+// --------------------------counts digit------------
+
+const nums = 34125;
+
+// expected ans = 5
+
+function countDigit(num) {
+    
+}
+
+
+// ------------------------Count vowels----------
+
+const str = "Hello World";
+
+// expected ans = 3
+
+function countVowels(str) {
+    
+}
+
+
+// -------------------------calculate square root
+
+const numsArr = [49, 64, 81, 16];
+
+// expected ans = [7, 8, 9, 4]
+
+function squareRoot(numsArr) {
+    
+}
+
+
+
+// ------------------------frequency of each letter-------
+
+const message = "Hello World";
+
+// expected ans - print frequency of each letter
+
+function calculateFrequency(message) {
+    
+}
+
+
+
+// ------------------------Sum of number elements
+
+const list = [1, 2, 3, "3476", 6, "43874", "2178832"];
+
+// eexpected ans = 12 
+
+function sumOfNumbers(list) {
+    
+}
+
+
+// ------------------------------sum of all even digit
+
+const number = 376347;
+
+// expected ans = 10
+
+
+// ---------------------Capitalize the first letter of each word
+
+const str1 = "The blue fox jumps over a white lazy dog";
+
+// expected ans = "The Blue Fox Jumps Over A White Lazy Dog";
+
+
+
+
