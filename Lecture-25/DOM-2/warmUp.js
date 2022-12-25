333f8afd9147ff6ceca9312a55258e802f581c09
Sabeel Khan <sabeelhps@gmail.com>
2022-12-25 10:42:40 +0530

    Lecture-25 (DOM-2, Events)

---
+++ Lecture-25/DOM-2/warmUp.js
@@ -0,0 +1,11 @@
+
+const IMAGE1 = "https://images.unsplash.com/photo-1669836053928-5b90afddd893?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDYzfEpwZzZLaWRsLUhrfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60";
+const IMAGE2 = "https://images.unsplash.com/photo-1669836068321-14a727c76d36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDY0fEpwZzZLaWRsLUhrfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60";
+const IMAGE3 = "https://images.unsplash.com/photo-1670271080081-bd6e50d8c195?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDU5fEpwZzZLaWRsLUhrfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60";
+const IMAGE4 = "https://images.unsplash.com/photo-1669830512962-6624050ffd6a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDY2fEpwZzZLaWRsLUhrfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60";
+
+function changeImageUtil(...images) {
+    
+}
+
+changeImageUtil(IMAGE1,IMAGE2,IMAGE3,IMAGE4);
