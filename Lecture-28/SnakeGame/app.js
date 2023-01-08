439ca0c0a6e4328fccbb9a6526498e565a0fd4c9
Sabeel Khan <sabeelhps@gmail.com>
2023-01-08 23:25:32 +0530

    SnakeGame

--- Lecture-28/SnakeGame/app.js
+++ Lecture-28/SnakeGame/app.js
@@ -23,30 +23,81 @@ class Snake{
         for (let i = 0; i < this.cells.length; i++) {
             const cell = this.cells[i];
             pen.fillStyle = i === this.cells.length - 1 ? 'red' : 'yellow';
-            pen.fillRect(cell.x * this.cellSize, cell.y * this.cellSize, this.cellSize-2, this.cellSize-2);
+            pen.fillRect(cell.x * this.cellSize, cell.y * this.cellSize, this.cellSize - 2, this.cellSize - 2);
         }
     }
 
-    updateSnake() {
+    getNextCellCoordinate() {
         const headX = this.cells[this.cells.length - 1].x;
         const headY = this.cells[this.cells.length - 1].y;
 
-        const nextX = headX + 1;
-        const nextY = headY;
+        let nextX;
+        let nextY;
 
+        switch (this.direction) {
+            case 'left':
+                nextX = headX - 1;
+                nextY = headY;
+                break;
+            case 'up':
+                nextX = headX;
+                nextY = headY - 1;
+                break;
+            case 'down':
+                nextX = headX;
+                nextY = headY + 1;
+                break;
+            default:
+                nextX = headX + 1;
+                nextY = headY;
+        }
+
+        return { x: nextX, y: nextY };
+    }
+
+    updateSnake(collision) {
+        const nextCellCoordinates = this.getNextCellCoordinate();
         // removes first cell from the cells array
-        this.cells.shift();
-
+        if (!collision) {
+            this.cells.shift();
+        }
         // add new cell after the current head
-        this.cells.push({ x: nextX, y: nextY });
+        this.cells.push(nextCellCoordinates);
+    }
+
+    getSnakeHead() {
+        const headX = this.cells[this.cells.length - 1].x;
+        const headY = this.cells[this.cells.length - 1].y;
+
+        return { x: headX, y: headY };
+    }
+}
+
+class Food{
+    constructor() {
+        this.foodSize = 67;
+        this.x = 0;
+        this.y = 0;
+    }
+
+    getRandomFood() {
+        this.x = Math.floor(Math.random() * (CANVAS_WIDTH - this.foodSize)/this.foodSize);
+        this.y = Math.floor(Math.random() * (CANVAS_HEIGHT - this.foodSize)/this.foodSize);
+    }
+
+    drawFood() {
+        pen.fillStyle = 'aqua';
+        pen.fillRect(this.x * this.foodSize, this.y * this.foodSize, this.foodSize, this.foodSize);
     }
 }
 
 const snake = new Snake();
+const food = new Food();
 
 function init() {
     snake.createSnake();
     snake.drawSnake();
+    food.getRandomFood();
 
     function keyPressed(event) {
         switch (event.key) {
@@ -69,11 +120,26 @@ function init() {
 
 function draw() {
     pen.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    food.drawFood();
     snake.drawSnake();
+    const nextCellCoordinates = snake.getNextCellCoordinate();
+    if (nextCellCoordinates.x * snake.cellSize - snake.cellSize >= CANVAS_WIDTH) {
+        pen.font = 'bold 48px serif';
+        pen.fillStyle = 'red';
+        pen.fillText('Game Over', 100, 100);
+        clearInterval(id);
+    }
 }
 
 function update() {
-    snake.updateSnake();
+    const snakeHead = snake.getSnakeHead();
+    let collision = false;
+    if (snakeHead.x === food.x && snakeHead.y === food.y) {
+        // collision condition meets for snake and food
+        collision = true;
+        food.getRandomFood();
+    }
+    snake.updateSnake(collision);
 }
 
 function gameLoop() {
